cc4b07a3b1f10ae085b436d34a645417acce9c1f
Zaleth Valentina Rivas Calderón <zvrivasca@unitru.edu.pe>
2025-12-19 01:47:38 -0500

    Modificar plantilla base

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,12 @@
 @extends('layouts.admin')
+
+@section('content')
+<div class="row h-100">
+    <div class="col-12 h-100 d-flex align-items-center justify-content-center">
+        <div class="text-center">
+            <h1 class="display-4">Bienvenido al Dashboard de Administrador</h1>
+            <p class="lead">Aquí puedes gestionar el sistema de votación.</p>
+        </div>
+    </div>
+</div>
+@endsection
